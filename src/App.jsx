036d59598aea87af089716e1c289d0b6bedefb25
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-08-19 18:05:56 +0200

    add products list

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  const [products, setProducts] = useState([
+    { name: "Latte", price: 1.5, category: "Bevande" },
+    { name: "Pane", price: 1.0, category: "Alimentari" },
+    { name: "Olio", price: 5.0, category: "Condimenti" },
+  ]);
+
+  const [inputNameValue, setInputNameValue] = useState("");
+  const [inputPriceValue, setInputPriceValue] = useState("");
+  const [inputCategoryValue, setInputCategoryValue] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("Tutti");
+
+  return (
+    <div className="app-container">
+      <div className="products">
+        <label>Inserisci nuovo prodotto</label>
+        <input
+          type="text"
+          name="name"
+          value={inputNameValue}
+          onChange={(e) => {
+            setInputNameValue(e.target.value);
+          }}
+          placeholder="inserisci il nome del prodotto"
+        />
+
+        <input
+          type="text"
+          name="price"
+          value={inputPriceValue}
+          onChange={(e) => {
+            setInputPriceValue(e.target.value);
+          }}
+          placeholder="inserisci il prezzo del prodotto"
+        />
+        <select
+          name="category"
+          placeholder="inserisci la cateogria del prodotto"
+          value={inputCategoryValue}
+          onChange={(e) => {
+            setInputCategoryValue(e.target.value);
+          }}
+        >
+          <option value="Alimentari">Alimentari</option>
+          <option value="Bevande">Bevande</option>
+          <option value="Condimenti">Condimenti</option>
+        </select>
+
+        <button
+          className="btn"
+          onClick={() => {
+            const newProduct = {
+              name: inputNameValue,
+              price: inputPriceValue,
+              category: inputCategoryValue,
+            };
+            const newProducts = [...products, newProduct];
+            setProducts(newProducts);
+          }}
+        >
+          Aggiungi nuovo prodotto
+        </button>
+      </div>
+      <div>
+        <label>Filtra per </label>
+        <select
+          name="category"
+          value={selectedCategory}
+          onChange={(e) => {
+            setSelectedCategory(e.target.value);
+          }}
+        >
+          <option value="Tutti">Tutti</option>
+          <option value="Alimentari">Alimentari</option>
+          <option value="Bevande">Bevande</option>
+          <option value="Condimenti">Condimenti</option>
+        </select>
+      </div>
+
+      <ul>
+        {products
+          /* .filter(
+            (product) =>
+              selectedCategory === "Tutti" ||
+              product.category === selectedCategory
+          ) */
+          .map(
+            (product, currentIndex) =>
+              (selectedCategory === "Tutti" ||
+                product.category === selectedCategory) && (
+                <li key={currentIndex}>
+                  {product.name}, {product.price}, {product.category}
+                </li>
+              )
+          )}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
